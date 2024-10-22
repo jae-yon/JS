70d66462138bd867adc9484cfd89e78f8f88dd9e
JaeYoung <kimjaeyoung650@gmail.com>
2024-10-22 19:16:06 +0900

    ADD: tailwindcss module

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 require('dotenv').config({
    path: path.join(__dirname, '/config/.env') 
 });
